9a32782da11a4f2c6b3f214da445ec9ad8e0ccbd
khushbu <0109khushbu@gmail.com>
2024-07-01 23:04:38 -0400

    link different pages from landing page

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'landingPage'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('shop/', views.shop, name='shop'),
