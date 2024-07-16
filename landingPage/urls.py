35fa5c8f41f4221c5ab09eccd78c793c4e292483
khushbu <0109khushbu@gmail.com>
2024-07-16 14:31:29 -0400

    fix add_to_cart issue by adding its url

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -28,4 +28,7 @@ urlpatterns = [
     path('password_reset/confirm/', views.password_reset_confirm, name='password_reset_confirm'),
     path('search/', views.search, name='search'),
     #path('order/<int:order_id>/', auth_middleware(views.order_detail), name='order_detail'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update_cart_quantity/<int:product_id>/<str:action>/', views.update_cart_quantity, name='update_cart_quantity'),
 ]
