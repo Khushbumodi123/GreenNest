460ee1dfae076625b5ea1f5874533ea2865a6090
khushbu <0109khushbu@gmail.com>
2024-07-16 16:37:47 -0400

    fix and map customer in seller model

--- seller_page/models.py
+++ seller_page/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from landingPage.models import Customer
 
 class Seller(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(Customer, on_delete=models.CASCADE)
     store_name = models.CharField(max_length=100)
 
 class Category(models.Model):
