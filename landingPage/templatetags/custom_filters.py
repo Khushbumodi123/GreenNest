ca9c222d44e3b45c357b3af8e626cd9babc36d75
khushbu <0109khushbu@gmail.com>
2024-07-18 12:18:24 -0400

    fix product quantity in cart in nav bar

--- landingPage/templatetags/custom_filters.py
+++ landingPage/templatetags/custom_filters.py
@@ -8,3 +8,7 @@ def truncate_with_ellipsis(value, max_length):
         return value[:max_length] + '...'
     else:
         return value[:max_length]      
+
+@register.filter
+def cart_total_quantity(cart):
+    return sum(cart.values())
