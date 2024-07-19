8d6a947a282bc9e2c921e4b381f7b248b1b7f2bb
DakshPrajapati <dakshprajapati882001>
2024-07-19 18:04:42 -0400

    pswd custom_filter updated

--- landingPage/templatetags/custom_filters.py
+++ landingPage/templatetags/custom_filters.py
@@ -6,4 +6,11 @@ register = template.Library()
 def truncate_with_ellipsis(value, max_length):
     if len(value) > max_length:
         return value[:max_length] + '...'
-    return value
+    else:
+        return value[:max_length]      
+
+@register.filter
+def cart_total_quantity(cart):
+    if cart:
+        return sum(cart.values())
+    return 0
